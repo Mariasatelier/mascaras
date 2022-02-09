e39091b6870ff7f5663f06843c347ee31cec5ac3
Mariasatelier <91706311+Mariasatelier@users.noreply.github.com>
2022-02-09 11:24:05 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,137 @@
+var itemCmp = document.querySelector('.item-content');
+var imgItem = document.querySelector('.img-item');
+var titleItem = document.querySelector('.title-item');
+var valorItem = document.querySelector('.valor-item');
+var pacelaItem = document.querySelector('.pacela-item');
+var descricaoItem = document.querySelector('.descricao-item');
+var bxX = document.querySelector('.bx-x');
+var cdp = document.querySelector('.cdp');
+var body = document.querySelector('body');
+var item1 = document.querySelector('.item-1');
+var item2 = document.querySelector('.item-2');
+var item3 = document.querySelector('.item-3');
+var item4 = document.querySelector('.item-4');
+var item5 = document.querySelector('.item-5');
+var item6 = document.querySelector('.item-6');
+var item7 = document.querySelector('.item-7');
+var item8 = document.querySelector('.item-8');
+
+item1.addEventListener('click', function(){
+  itemCmp.style.display = "block"
+  body.style.overflow = "hidden"
+  bxX.addEventListener('click', function(){
+    itemCmp.style.display = "none"
+    body.style.overflow = "visible"
+  });
+  imgItem.src = "https://i.postimg.cc/N0hc1N9B/Whats-App-Image-2022-01-27-at-18-13-02-4-removebg-preview.png"
+  titleItem.innerHTML = "Máscara 3D com estampa de minnie - Tecido 100% algodão"
+  valorItem.innerHTML = "R$5,90"
+  pacelaItem.innerHTML = "Parcela: 1x sem juros"
+  descricaoItem.innerHTML = "Máscara 3D com estampa personalizada super confortavel com tecido 100% algodão."
+  cdp.value = "SPFC"
+});
+
+item2.addEventListener('click', function(){
+  itemCmp.style.display = "block"
+  body.style.overflow = "hidden"
+  bxX.addEventListener('click', function(){
+    itemCmp.style.display = "none"
+    body.style.overflow = "visible"
+  });
+  imgItem.src = "https://i.postimg.cc/286rmLNR/Whats-App-Image-2022-01-27-at-18-13-02-removebg-preview.png"
+  titleItem.innerHTML = "Máscara 3D com estampa de cor rosa - Tecido 100% algodão"
+  valorItem.innerHTML = "R$5,90"
+  pacelaItem.innerHTML = "Parcela: 1x sem juros"
+  descricaoItem.innerHTML = "Máscara 3D com estampa de cor rosa super confortavel com tecido 100% algodão."
+  cdp.value = "IBIS"
+});
+
+item3.addEventListener('click', function(){
+  itemCmp.style.display = "block"
+  body.style.overflow = "hidden"
+  bxX.addEventListener('click', function(){
+    itemCmp.style.display = "none"
+    body.style.overflow = "visible"
+  });
+  imgItem.src = "https://i.postimg.cc/rFPcpyqd/Whats-App-Image-2022-01-27-at-18-13-03-2-removebg-preview.png"
+  titleItem.innerHTML = "Máscara 3D com estampa personalizada de minions - Tecido 100% algodão"
+  valorItem.innerHTML = "R$5,90"
+  pacelaItem.innerHTML = "Parcela: 1x sem juros"
+  descricaoItem.innerHTML = "Máscara 3D com estampa personalizada dos minions super confortavel com tecido 100% algodão."
+  cdp.value = "4EFD"
+});
+
+item4.addEventListener('click', function(){
+   itemCmp.style.display = "block"
+  body.style.overflow = "hidden"
+  bxX.addEventListener('click', function(){
+    itemCmp.style.display = "none"
+    body.style.overflow = "visible"
+  });
+  imgItem.src = "https://i.postimg.cc/13VSVxzv/Whats-App-Image-2022-01-27-at-18-13-04-removebg-preview.png"
+  titleItem.innerHTML = "Máscara 3D com estampa personalizada do homem aranha - Tecido 100% algodão"
+  valorItem.innerHTML = "R$5,90"
+  pacelaItem.innerHTML = "Parcela: 1x sem juros"
+  descricaoItem.innerHTML = "Máscara 3D com estampa personalizada do homem aranha super confortavel com tecido 100% algodão"
+  cdp.value = "UIYV"
+});
+
+item5.addEventListener('click', function(){
+   itemCmp.style.display = "block"
+  body.style.overflow = "hidden"
+  bxX.addEventListener('click', function(){
+    itemCmp.style.display = "none"
+    body.style.overflow = "visible"
+  });
+  imgItem.src = "https://i.postimg.cc/43SDpfpL/Whats-App-Image-2022-01-27-at-18-13-02-1-removebg-preview.png"
+  titleItem.innerHTML = "Máscara 3d com estampa de cor branca com - Tecido 100% algodão"
+  valorItem.innerHTML = "R$5,90"
+  pacelaItem.innerHTML = "Parcela: 1x sem juros"
+  descricaoItem.innerHTML = "Máscara 3D com estampa de cor branca super confortavel com tecido 100% algodão"
+  cdp.value = "UCGU"
+});
+
+item6.addEventListener('click', function(){
+   itemCmp.style.display = "block"
+  body.style.overflow = "hidden"
+  bxX.addEventListener('click', function(){
+    itemCmp.style.display = "none"
+    body.style.overflow = "visible"
+  });
+  imgItem.src = "https://i.postimg.cc/4dHr5D5J/Whats-App-Image-2022-01-27-at-18-13-02-2-removebg-preview.png"
+  titleItem.innerHTML = "Máscara 3d com estampa de cor preta com - Tecido 100% algodão"
+  valorItem.innerHTML = "R$5,90"
+  pacelaItem.innerHTML = "Parcela: 1x sem juros"
+  descricaoItem.innerHTML = "Máscara 3D com estampa de cor preta super confortavel com tecido 100% algodão"
+  cdp.value = "KJJY"
+});
+
+item7.addEventListener('click', function(){
+   itemCmp.style.display = "block"
+  body.style.overflow = "hidden"
+  bxX.addEventListener('click', function(){
+    itemCmp.style.display = "none"
+    body.style.overflow = "visible"
+  });
+  imgItem.src = "https://i.postimg.cc/BZDr1Gbp/Whats-App-Image-2022-01-27-at-18-13-02-3-removebg-preview.png"
+  titleItem.innerHTML = "Máscara 3D com estampa de cor amarela - Tecido 100% algodão"
+  valorItem.innerHTML = "R$5,90"
+  pacelaItem.innerHTML = "Parcela: 1x sem juros"
+  descricaoItem.innerHTML = "Máscara 3D com estampa de cor amarela super confortavel com tecido 100% algodão"
+  cdp.value = "JUCA"
+});
+
+item8.addEventListener('click', function(){
+  itemCmp.style.display = "block"
+  body.style.overflow = "hidden"
+  bxX.addEventListener('click', function(){
+    itemCmp.style.display = "none"
+    body.style.overflow = "visible"
+  });
+  imgItem.src = "https://i.postimg.cc/Fzjwt088/Whats-App-Image-2022-01-27-at-18-13-03-removebg-preview.png"
+  titleItem.innerHTML = "Máscara 3D com estampa personalizada dos carros - Tecido 100% algodão"
+  valorItem.innerHTML = "R$5,90"
+  pacelaItem.innerHTML = "Parcela: 1x sem juros"
+  descricaoItem.innerHTML = "Máscara 3D com estampa personalizada dos carros super confortavel com tecido 100% algodão"
+  cdp.value = "KJUC"
+});
